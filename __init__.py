e3f9ec82f3c8c7318b1ae0bdfa81180053da2ecc
aloys <aloysiuslow009@gmail.com>
2024-05-22 13:12:14 +0800

    Added in function names and home.html template

--- __init__.py
+++ __init__.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for,  session, flash
-
+from flask import Flask, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_super_secret_key'
+
+mysql = MySQL(app)
+#Aloysius Portion
+def input_validation():
+    pass
+def get_info():
+    pass
+
+
+def update_info():
+    pass
+
+def add_info():
+    pass
+
+def delete_info():
+    pass
+
+
+
+
+
 
 
 @app.route('/')
