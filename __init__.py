e3c429a13ea1956b5035101427bf156223f72c3e
aloys <aloysiuslow009@gmail.com>
2024-05-22 13:34:29 +0800

    Added in function names and home.html template

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, jsonify
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -21,6 +21,29 @@ def add_info():
 def delete_info():
     pass
 
+def create_table():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        query = """
+        CREATE TABLE IF NOT EXISTS users(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            age INT NOT NULL
+            address VARCHAR(255),
+            role ENUM('student','teacher','admin') NOT NULL
+        )
+        """
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Table created")
+
+    except Exception as e:
+        print(f"Error occurred: {str(e)}")
 
 
 
