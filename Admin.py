b3ec0b008ea1717a181c5248d53558e8804b51ba
jyoshwlitha@gmail.com <jyoshwlitha@gmail.com>
2024-08-12 15:14:43 +0800

    fixing bug in admin update password

--- Admin.py
+++ Admin.py
@@ -165,69 +165,61 @@ def adminUpdatePassword():
             print("Session data:", session['user'])  # Debug statement
             print("Username from session:", username)  # Debug statement
 
-            with mydb.cursor() as mycursor:
-                if request.method == 'POST':
-                    new_password = request.form.get('new_password')
-                    confirm_password = request.form.get('confirm_password')
+            if request.method == 'POST':
+                new_password = request.form.get('new_password')
+                confirm_password = request.form.get('confirm_password')
 
-                    if new_password and confirm_password:
+                if new_password and confirm_password:
+                    if new_password == confirm_password:
                         try:
-                            if new_password == confirm_password:
-                                hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+                            hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
 
-                                # Check if the new hashed password already exists in the database
-                                print(
-                                    "Checking if the new hashed password already exists in the database.")  # Debug statement
-                                mycursor.execute("SELECT password FROM users")
-                                all_passwords = mycursor.fetchall()
+                            # Check if the new hashed password already exists in the database
+                            print("Checking if the new hashed password already exists in the database.")  # Debug statement
+                            mycursor.execute("SELECT password FROM users")
+                            all_passwords = mycursor.fetchall()
 
-                                # Check if the new password matches any existing password
-                                password_exists = False
-                                for stored_password in all_passwords:
-                                    if bcrypt.checkpw(new_password.encode('utf-8'), stored_password[0].encode('utf-8')):
-                                        password_exists = True
-                                        break
+                            # Check if the new password matches any existing password
+                            password_exists = False
+                            for stored_password in all_passwords:
+                                if bcrypt.checkpw(new_password.encode('utf-8'), stored_password[0].encode('utf-8')):
+                                    password_exists = True
+                                    break
 
-                                if password_exists:
-                                    flash('Password already exists. Please create another password', 'danger')
+                            if password_exists:
+                                flash('Password already exists. Please create another password', 'danger')
+                                if 'user' in session and 'id' in session['user']:
+                                    log_this("Existing password exists when creating a password")
+                                return redirect(url_for('adminUpdatePassword'))
+                            else:
+                                try:
+                                    print(f"Updating password for username: {username}")  # Debug statement
+                                    print(f"Hashed password: {hashed_password}")  # Debug statement
+                                    mycursor.execute("UPDATE users SET password = %s WHERE username = %s",
+                                                     (hashed_password, username))
+                                    mydb.commit()
+                                    flash('Password updated successfully', 'success')
                                     if 'user' in session and 'id' in session['user']:
-                                        log_this("Existing password exists when creating a password")
+                                        log_this("Password updated successfully")
+                                    print('Password updated successfully')  # Debug statement
+
+                                    # Get the user's email to send the notification
+                                    mycursor.execute("SELECT email FROM users WHERE username = %s", (username,))
+                                    email_result = mycursor.fetchone()
+                                    email = email_result[0]
+                                    print(email)
+
+                                    subject = 'Password Changed'
+                                    template = f'''<p>Dear user, <br><br>
+                                                    You have recently changed your password.<br><br>
+                                                    Yours, <br>
+                                                    PlanetExplorers Team</p>'''
+                                    send_reset_link_email(email, subject, template)
+
+                                except Exception as e:
+                                    flash(f'Error updating password: {str(e)}', 'danger')
+                                    print(f'SQL Update Error: {str(e)}')  # Debug statement
                                     return redirect(url_for('adminUpdatePassword'))
-                                else:
-                                    try:
-                                        print(f"Updating password for username: {username}")  # Debug statement
-                                        print(f"Hashed password: {hashed_password}")  # Debug statement
-                                        mycursor.execute("UPDATE users SET password = %s WHERE username = %s",
-                                                         (hashed_password, username))
-                                        mydb.commit()
-                                        flash('Password updated successfully', 'success')
-                                        if 'user' in session and 'id' in session['user']:
-                                            log_this("Password updated successfully")
-                                        print('Password updated successfully')  # Debug statement
-
-                                        # # Refresh session user data
-                                        # user = userSession(username)
-                                        # if user:
-                                        #     session['user'] = user  # Update session with refreshed user data
-                                        # else:
-                                        #     flash('User not found in database after update', 'error')
-                                        #     return redirect(url_for('login'))
-                                        mycursor.execute("SELECT email FROM users WHERE username = %s", (username,))
-                                        email_result = mycursor.fetchone()
-                                        email = email_result[0]
-                                        print(email)
-
-                                        subject = 'Password Changed'
-                                        template = f'''<p>Dear user, <br><br>
-                                                        You have recently changed your password.<br><br>
-                                                        Yours, <br>
-                                                        PlanetExplorers Team</p>'''
-                                        send_reset_link_email(email, subject, template)
-
-                                    except Exception as e:
-                                        flash(f'Error updating password: {str(e)}', 'danger')
-                                        print(f'SQL Update Error: {str(e)}')  # Debug statement
-                                        return redirect(url_for('adminUpdatePassword'))
 
                         except Exception as e:
                             flash(f'Error checking existing password: {str(e)}', 'danger')
@@ -246,8 +238,8 @@ def adminUpdatePassword():
         flash("User session not found")
         return redirect(url_for('login'))
 
-    mycursor.close()
     return render_template("Admin/adminUpdatePassword.html")
+
 
 
 @app.route('/adminCreateTeacher', methods=['GET', 'POST'])
